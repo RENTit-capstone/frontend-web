265f313e03a20135d0409ec7014a0f89511cd3e6
gurbur <jihwan7577@gmail.com>
2025-06-05 14:39:22 +0900

    Fix: removev unused library

--- src/components/cards/ChartCard.tsx
+++ src/components/cards/ChartCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { getData } from '../../api/requests';
 
