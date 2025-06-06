11067d714bd705342c28a1a9f806a027c52049e8
gurbur <jihwan7577@gmail.com>
2025-06-07 01:41:01 +0900

    Fix: make dynamic api url to be real dynamic

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,20 +1,61 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import useAuthStore from '../stores/useAuthStore';
 
+let axiosInstance: AxiosInstance | null = null;
+let axiosNoInterceptor: AxiosInstance | null = null;
+
 const getBaseUrl = () => window.__ENV__?.VITE_API_BASE_URL || '/';
+console.log("test", getBaseUrl());
 
-export const axiosNoInterceptor = axios.create({
-    baseURL: getBaseUrl(),
-});
+const getAxiosNoInterceptor = (): AxiosInstance => {
+    if (!axiosNoInterceptor) {
+        axiosNoInterceptor = axios.create({
+            baseURL: getBaseUrl(),
+        });
+    }
+    return axiosNoInterceptor
+};
 
-export const axiosInstance = axios.create({
-    baseURL: getBaseUrl(),
-});
+const getAxiosInstance = (): AxiosInstance => {
+    if (!axiosInstance) {
+        axiosInstance = axios.create({
+            baseURL: getBaseUrl(),
+        });
+
+        axiosInstance.interceptors.request.use((config) => {
+            const token = useAuthStore.getState().accessToken;
+            if (token) {
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+            config.headers['Content-Type'] = 'application/json';
+            return config;
+        });
+
+        axiosInstance.interceptors.response.use(
+            (response) => response,
+            async (error) => {
+                const originalRequest = error.config;
+                if (error.response?.status === 403 && !originalRequest._retry) {
+                    originalRequest._retry = true;
+                    try {
+                        const newAccessToken = await getNewToken();
+                        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+                        return getAxiosInstance()(originalRequest);
+                    } catch (err) {
+                        return Promise.reject(err);
+                    }
+                }
+                return Promise.reject(error);
+            }
+        );
+    }
+    return axiosInstance;
+}
 
 const getNewToken = async () => {
     const { accessToken, refreshToken } = useAuthStore.getState();
     const payload = { accessToken, refreshToken };
-    const response = await axiosNoInterceptor.post('/api/v1/auth/login/refresh', payload);
+    const response = await getAxiosNoInterceptor().post('/api/v1/auth/login/refresh', payload);
 
     if (response.data.success) {
         const { accessToken: newAccessToken, refreshToken: newRefreshToken, memberId } = response.data.data;
@@ -26,38 +67,4 @@ const getNewToken = async () => {
     }
 };
 
-axiosInstance.interceptors.request.use(
-    (config) => {
-        config.baseURL = getBaseUrl();
-        const token = useAuthStore.getState().accessToken;
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        config.headers['Content-Type'] = 'application/json';
-        console.log(`request config ${config.url} :`, config);
-        return config;
-    },
-    (error) => Promise.reject(error)
-);
-
-axiosInstance.interceptors.response.use(
-    (response) => {
-        console.log(`response ${response.config.url}: `,response);
-        return response
-    },
-    async (error) => {
-        const originalRequest = error.config;
-        if (error.response?.status === 403 && !originalRequest._retry) {
-            originalRequest._retry = true;
-            try {
-                const newAccessToken = await getNewToken();
-                originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-                return axiosInstance(originalRequest);
-            } catch (err) {
-                useAuthStore.getState().clearTokens();
-                return Promise.reject(err);
-            }
-        }
-        return Promise.reject(error);
-    }
-);
+export { getAxiosInstance, getAxiosNoInterceptor };
